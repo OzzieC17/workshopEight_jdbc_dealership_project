a692dfcfc3b0cccd9b5543d62717d86876abc8c8
ozzie <ocarreon@appdev.yearup.org>
2025-06-15 16:25:22 -0400

    classes added

--- src/main/java/com/ps/dealership/Main.java
+++ src/main/java/com/ps/dealership/Main.java
@@ -1,4 +1,9 @@
 package com.ps.dealership;
 
 public class Main {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.display();
+
+    }
 }
